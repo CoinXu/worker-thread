5dd2eb22d4a0fedcf6bd66164e2c81bf270d3318
coinxu <duanxian0605@gmail.com>
2017-11-21 10:02:03 +0800

    user worker-loader

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # worker-thread
+
+# Usage
+First, you need webpack help you manager worker dependencies.
+```js
+// webpack.config.js
+{
+  module: {
+    loaders: [
+      {
+        test: /worker\.js$/,
+        use: { loader: 'worker-loader' }
+      }
+    ]
+  }
+}
+```
+
+Second, create a worker JavaScript module.
+```js
+// worker.js
+import Injector from '../src/Injector'
+const injector = new Injector(self)
+injector.on('message', function (message) {
+  console.log('received: %s', message)
+})
+
+injector.on('error', function (error) {
+  console.log('received error: %s', error)
+})
+```
+
+Final, use worker in main thread.
+```js
+import Manager from '../src/Manager'
+import SampleWorker from './worker'
+
+const manager = new Manager(new SampleWorker())
+
+manager.on('message', function (message) {
+  console.log('message %s', message)
+})
+
+manager.on('error', function (message) {
+  console.log('received error: %s', message)
+})
+
+manager.send('message', 'sample message')
+```
+
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+/**
+ * @Author sugo.io<asd>
+ * @Date 17-9-22
+ */
+
+const path = require('path')
+const pkg = require('./package.json')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    main: './sample/main'
+  },
+  output: {
+    path: path.join(process.cwd(), './dist'),
+    publicPath: '/worker-thread/dist/',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /worker\.js$/,
+        use: { loader: 'worker-loader' }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      // name: 'vender', // Move dependencies to our vender file
+      children: true, // Look for common dependencies in all children,
+      async: true,
+      minChunks: 2 // How many times a dependency must come up before being extracted
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '\'' + process.env.NODE_ENV + '\'',
+        'VERSION': '\'' + pkg.version + '\''
+      }
+    })
+  ]
+}
